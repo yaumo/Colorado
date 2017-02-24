04f9fcce1b04306a1c0cbaa5aceb6317147c8288
Path0g3n <thomasjohnrichter@web.de>
2017-02-24 13:12:29 +0100

    Added new Model entities. Missing setters/getters

---
+++ src/com/colorado/denver/model/BaseEntity.java
@@ -0,0 +1,5 @@
+package com.colorado.denver.model;
+
+public class BaseEntity {
+
+}
---
+++ src/com/colorado/denver/model/Task.java
@@ -0,0 +1,7 @@
+package com.colorado.denver.model;
+
+public class Task extends BaseEntity {
+	public static final String TITLE = "title";
+	public static final String DESCRIPTION = "description";
+
+}
