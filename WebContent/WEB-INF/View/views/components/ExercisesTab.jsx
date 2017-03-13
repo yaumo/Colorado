6a6d8468315fac73f0017087458ef47b23ef8e6d
Nico H <nico.himmelein@web.de>
2017-03-13 13:03:21 +0100

    Add ExerciseTab and OverviewTab

--- WebContent/WEB-INF/View/views/components/ExercisesTab.jsx
+++ WebContent/WEB-INF/View/views/components/ExercisesTab.jsx
@@ -1,17 +1,67 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import Divider from 'material-ui/Divider';
+import Solution from './Solution.jsx';
+import TextField from 'material-ui/TextField';
+import DatePicker from 'material-ui/DatePicker';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+
+
+function handleChange(event, index, value){
+    this.setState({value});
+};
 
 class ExercisesTab extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            open: true,
+            value: 1
+        };
+    }
     render() {
         return (
             <div>
-                <h2>Exercises Tab</h2>
-                <p>
-                    This is an example tab.
-                </p>
-                <p>
-                    You can put any sort of HTML or react component in here. It even keeps the component state!
-                </p>
+                <Card>
+                    <CardHeader
+                        title="New Exercise"
+                        />
+                    <Divider />
+                    <CardText>
+                        <TextField
+                            floatingLabelText="Title"
+                            fullWidth={false}
+                        />
+                        <TextField
+                                floatingLabelText="Exercise"
+                                multiLine={true}
+                                rows={3}
+                                fullWidth={true}
+                        /><br />
+                        <br />
+                        <h4>Solution:</h4>
+                        <Solution />
+                    </CardText>
+                </Card>
+                <br />
+                <Card>
+                    <CardHeader
+                        title="Append Exercises to Course"
+                        />
+                    <Divider />
+                    <CardText>
+                     <DropDownMenu value={this.state.value} onChange={this.handleChange}>
+                                <MenuItem value={1} primaryText="Webprogrammierung" />
+                                <MenuItem value={2} primaryText="Datenbanken" />
+                                <MenuItem value={3} primaryText="Programmieren 1" />
+                            </DropDownMenu>
+                        <DatePicker floatingLabelText="Deadline" container="inline" mode="landscape" />
+                        <br />
+                    </CardText>
+                </Card>
             </div>
         );
     }
--- WebContent/WEB-INF/View/views/components/OverviewTab.jsx
+++ WebContent/WEB-INF/View/views/components/OverviewTab.jsx
@@ -1,17 +1,78 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import Divider from 'material-ui/Divider';
+import { Table, TableBody, TableFooter, TableHeader, TableHeaderColumn, TableRow, TableRowColumn }
+    from 'material-ui/Table';
+
+
+const tableData = [
+    {
+        name: 'John Smith',
+        status: 'Employed',
+    },
+    {
+        name: 'Randal White',
+        status: 'Unemployed',
+    },
+    {
+        name: 'Stephanie Sanders',
+        status: 'Employed',
+    },
+    {
+        name: 'Steve Brown',
+        status: 'Employed',
+    },
+    {
+        name: 'Joyce Whitten',
+        status: 'Employed',
+    },
+    {
+        name: 'Samuel Roberts',
+        status: 'Employed',
+    },
+    {
+        name: 'Adam Moore',
+        status: 'Employed',
+    },
+];
 
 class OverviewTab extends React.Component {
     render() {
         return (
             <div>
-                <h2>Tab One</h2>
-                <p>
-                    This is an Overview tab.
-                </p>
-                <p>
-                    You can put any sort of HTML or react component in here. It even keeps the component state!
-                </p>
+                <Card>
+                    <CardHeader
+                        title="Overview"
+                        />
+                    <Divider />
+                    <CardText>
+                        <Table
+                            >
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHeaderColumn colSpan="3" style={{ textAlign: 'center' }}>
+                                        Super Header
+                                    </TableHeaderColumn>
+                                </TableRow>
+                                <TableRow>
+                                    <TableHeaderColumn>ID</TableHeaderColumn>
+                                    <TableHeaderColumn>Name</TableHeaderColumn>
+                                    <TableHeaderColumn>Status</TableHeaderColumn>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {tableData.map((row, index) => (
+                                    <TableRow key={index} selected={row.selected}>
+                                        <TableRowColumn>{index}</TableRowColumn>
+                                        <TableRowColumn>{row.name}</TableRowColumn>
+                                        <TableRowColumn>{row.status}</TableRowColumn>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </CardText>
+                </Card>
             </div>
         );
     }
