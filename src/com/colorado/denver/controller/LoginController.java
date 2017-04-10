f61615603a40685c71237aa4de8608b8d2a28e1f
Path0g3n <thomasjohnrichter@web.de>
2017-04-10 14:26:29 +0200

    some fixes to security

--- src/com/colorado/denver/controller/LoginController.java
+++ src/com/colorado/denver/controller/LoginController.java
@@ -1,15 +1,11 @@
 package com.colorado.denver.controller;
 
 import java.io.IOException;
-import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
@@ -17,9 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpServerErrorException;
 
-import com.colorado.denver.model.User;
 import com.colorado.denver.services.SecurityServiceImpl;
 import com.colorado.denver.tools.DenverConstants;
 
@@ -35,28 +29,8 @@ public class LoginController extends HttpServlet {
 	@ResponseBody
 	public void handleLoginRequest(HttpServletRequest request,
 			HttpServletResponse response, Authentication authentication) throws IOException {
-		JSONObject jsonObject = null;
 		System.out.println("HALLO bin im login!");
-		String jsonStr = DenverConstants.ERROR_NO_OBJECT_FROM_REQUEST;
-		String username = "";
-		String password = "";
-		try {
-			jsonStr = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-			jsonObject = new JSONObject(jsonStr);
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new HttpServerErrorException(HttpStatus.BAD_REQUEST);
-		}
 
-		try {
-			username = jsonObject.getString(User.USERNAME);
-			password = jsonObject.getString(User.PASSWORD);
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println(username);
-		System.out.println(password);
 		SecurityServiceImpl impl = new SecurityServiceImpl();
 
 		impl.authenticate(authentication);
--- src/com/colorado/denver/services/security/SecurityConfig.java
+++ src/com/colorado/denver/services/security/SecurityConfig.java
@@ -27,18 +27,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-
-		http.authorizeRequests().antMatchers("/assests/**").permitAll()
-				.antMatchers("/admin").access("hasRole('ADMIN')")
-				.antMatchers("/member").access("hasRole('ADMIN') and hasRole('MEMBER')")
-				.and()
-				.formLogin().loginPage("/loginPage")
-				.defaultSuccessUrl("/homePage")
-				.failureUrl("/loginPage?error")
-				.usernameParameter("userName").passwordParameter("password")
-				.and().csrf()
-				.and()
-				.logout().logoutSuccessUrl("/loginPage?logout");
+		http.csrf().disable();
+		/*
+		 * http.authorizeRequests().antMatchers("/assests/**").permitAll()
+		 * .antMatchers("/admin").access("hasRole('ADMIN')")
+		 * .antMatchers("/member").access("hasRole('ADMIN') and hasRole('MEMBER')")
+		 * .and()
+		 * .formLogin().loginPage("/loginPage")
+		 * .defaultSuccessUrl("/homePage")
+		 * .failureUrl("/loginPage?error")
+		 * .usernameParameter("userName").passwordParameter("password")
+		 * .and().csrf()
+		 * .and()
+		 * .logout().logoutSuccessUrl("/loginPage?logout");
+		 */
 
 	}
 
