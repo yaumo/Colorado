3cbcab626aeced50ca21262c036b660fe5eb5972
Path0g3n <thomasjohnrichter@web.de>
2017-04-11 16:21:22 +0200

    Removed possible problem makers

--- src/com/colorado/denver/services/security/SecurityConfig.java
+++ src/com/colorado/denver/services/security/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.colorado.denver.services.SecurityServiceImpl;
 
@@ -20,22 +19,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth
-				.authenticationProvider(customAuthenticationProvider)
-				.userDetailsService(auth.getDefaultUserDetailsService())
-				.passwordEncoder(new BCryptPasswordEncoder());// This will probaply not work
+				.authenticationProvider(customAuthenticationProvider);// This will probaply not work
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.authorizeRequests().antMatchers("/").permitAll()
+		http.authorizeRequests().antMatchers("/**").permitAll()
 				.and()
 				.formLogin().loginPage("/login")
 				.defaultSuccessUrl("/exercise")
 				.failureUrl("/login?error")
 				.usernameParameter("username").passwordParameter("password")
 				.and().csrf().disable()
-				.logout().logoutSuccessUrl("/loginPage?logout");
+				.logout().logoutSuccessUrl("/login?logout");
 
 	}
 
