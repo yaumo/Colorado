f897c2344d2b5c30a63777a659485fe2ad12f0d7
Path0g3n <thomasjohnrichter@web.de>
2017-04-11 11:33:01 +0200

    Some changes to JSExecutor

--- src/com/colorado/denver/services/codeExecution/JavaScriptExecutor.java
+++ src/com/colorado/denver/services/codeExecution/JavaScriptExecutor.java
@@ -1,5 +1,8 @@
 package com.colorado.denver.services.codeExecution;
 
+import java.util.Iterator;
+import java.util.Set;
+
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptException;
@@ -13,6 +16,7 @@ import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
 public interface JavaScriptExecutor {
 	final static org.slf4j.Logger LOGGER = LoggerFactory.getLogger(JavaExecutor.class);
 
+	@SuppressWarnings("restriction")
 	public default String executeJavaScript(String inputType, String outputType, String excInput, String code) throws SecurityException {
 		String result = "";
 		try {
@@ -23,13 +27,23 @@ public interface JavaScriptExecutor {
 			Object results = invocable.invokeFunction("test", excInput);
 
 			// call that anon function
+			Set<String> keySet = object.keySet();
+			int i = 0;
+			for (Iterator iterator = keySet.iterator(); iterator.hasNext();) {
+				String content = (String) iterator.next();
+				System.out.println("Pos: " + i + "   Value: " + content);
+				i++;
+			}
+
 			System.out.println("Object way: " + object.call(null, excInput));
-			System.out.println("Invoke way: " + result);
-			System.out.println("Invoke way class: " + result.getClass());
+			System.out.println("Invoke way: " + results);
+			System.out.println("Invoke way class: " + results.getClass());
 			result = results.toString();
 		} catch (ScriptException e) {
+			LOGGER.error("Error executing script itself");
 			e.printStackTrace();
 		} catch (NoSuchMethodException e) {
+			LOGGER.error("No method");
 			e.printStackTrace();
 		}
 
