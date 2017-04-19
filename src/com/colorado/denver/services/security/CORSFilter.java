cc345651f3c71880a9c1bc0e2b1dfaff709ae977
Path0g3n <thomasjohnrichter@web.de>
2017-04-19 23:28:17 +0200

    Okay. WTF is this shit

--- src/com/colorado/denver/services/security/CORSFilter.java
+++ src/com/colorado/denver/services/security/CORSFilter.java
@@ -1,6 +1,8 @@
 package com.colorado.denver.services.security;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -8,6 +10,7 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.LoggerFactory;
@@ -15,6 +18,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CORSFilter implements Filter {
+
+	private final List<String> allowedOrigins = Arrays.asList("http://localhost:8080");
+
 	private final static org.slf4j.Logger LOGGER = LoggerFactory.getLogger(CORSFilter.class);
 
 	@Override
@@ -22,14 +28,22 @@ public class CORSFilter implements Filter {
 	}
 
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-		HttpServletResponse response = (HttpServletResponse) res;
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH");
-		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Authorization, *");
-		chain.doFilter(request, response);
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+		// Lets make sure that we are working with HTTP (that is, against HttpServletRequest and HttpServletResponse objects)
+		if (req instanceof HttpServletRequest && res instanceof HttpServletResponse) {
+			HttpServletRequest request = (HttpServletRequest) req;
+			HttpServletResponse response = (HttpServletResponse) res;
+
+			String origin = request.getHeader("Origin");
+			response.setHeader("Access-Control-Allow-Origin", allowedOrigins.contains(origin) ? origin : "");
+			response.setHeader("Vary", "Origin");
+			response.setHeader("Access-Control-Allow-Credentials", "true");
+			response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH");
+			response.setHeader("Access-Control-Max-Age", "3600");
+			response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Authorization, *");
+		}
+		chain.doFilter(req, res);
 	}
 
 	@Override
