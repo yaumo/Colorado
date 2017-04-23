603e3db7a73bf2124eb811296c03c0eea2f8501c
philippthiele <philipp.thiele.94@gmail.com>
2017-04-23 13:09:50 +0200

    Added /updatePassword API

--- src/com/colorado/denver/controller/UserInteractionController.java
+++ src/com/colorado/denver/controller/UserInteractionController.java
@@ -1,5 +1,6 @@
 package com.colorado.denver.controller;
 
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,7 @@ public class UserInteractionController {
 
 	@RequestMapping(value = "/registration", method = RequestMethod.POST)
 	public String registration(@RequestParam(value = "username", required = true) String username,
-			@RequestParam(value = "password", required = false) String password) {
+			@RequestParam(value = "password", required = true) String password) {
 		User usr = new User();
 		usr.setUsername(username);
 
@@ -30,13 +31,30 @@ public class UserInteractionController {
 
 		HibernateController hibCtrl = new HibernateController();
 		hibCtrl.addEntity(usr);
-		return "Created User: " + usr.getUsername() + "password: " + usr.getPassword();
+		return "Created User: " + usr.getUsername();
 	}
 
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public boolean login() {
 
 		return UserService.isCurrentUserDocent();
+	}
+	
+	public String changePassword(@RequestParam(value = "oldPassword", required = true) String oldPW,
+			@RequestParam(value = "newPassword", required = true) String newPW) {
+		User usr = UserService.getCurrentUser();
+		
+		if (!BCrypt.checkpw(oldPW, usr.getPassword())) {
+			throw new BadCredentialsException("Wrong password.");			
+		} else{
+			String salt = BCrypt.gensalt(12);
+			usr.setPassword(BCrypt.hashpw(newPW, salt));
+			
+			HibernateController hibCtrl = new HibernateController();
+			hibCtrl.mergeEntity(usr);
+			
+			return "Changed password of user: " + usr.getUsername();
+		}
 	}
 
 }
