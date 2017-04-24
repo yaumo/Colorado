eda81cf89a2b1a289dd295357b20544fdcee07df
Path0g3n <thomasjohnrichter@web.de>
2017-04-24 21:58:52 +0200

    other security approach. keeping it simple

--- src/com/colorado/denver/services/security/SecurityConfig.java
+++ src/com/colorado/denver/services/security/SecurityConfig.java
@@ -3,9 +3,7 @@ package com.colorado.denver.services.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,83 +13,31 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.colorado.denver.services.user.MyUserDetailsService;
 
-//@Configuration
+@Configuration
 @EnableWebSecurity
-public class SecurityConfig {
-
-	@Configuration
-	@Order(1)
-	public static class DocentSecurityConfig extends WebSecurityConfigurerAdapter {
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.csrf().disable()
-					.authorizeRequests()
-					.antMatchers("/docent/**").hasRole("DOCENT")
-					.and().httpBasic()
-					.and().sessionManagement()
-					.invalidSessionUrl("/logout");
-			// .and().addFilterAfter(new BasicAuthenticationFilter(authenticationManagerBean()), BasicAuthenticationFilter.class);
-
-			super.configure(http);
-		}
-
-		@Override
-		public void configure(WebSecurity web) throws Exception {
-			web.debug(true);
-		}
-	}
-
-	@Configuration
-	@Order(2)
-	public static class LoginRegistrationSecurityConfig extends WebSecurityConfigurerAdapter {
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.csrf().disable()
-					.authorizeRequests()
-					.antMatchers("/**").hasRole("STUDENT")
-					.and().httpBasic()
-					.and().sessionManagement().invalidSessionUrl("/logout");
-			// .and().addFilterAfter(new BasicAuthenticationFilter(authenticationManagerBean()), BasicAuthenticationFilter.class);
-
-			super.configure(http);
-		}
-
-		@Override
-		public void configure(WebSecurity web) throws Exception {
-			web.debug(true);
-		}
-
-	}
-
-	@Configuration
-	public static class StudentSecurityConfig extends WebSecurityConfigurerAdapter {
-
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.csrf().disable()
-					.authorizeRequests()
-					.antMatchers("/registration").permitAll()
-					.anyRequest().authenticated()
-					.and().httpBasic()
-					.and().sessionManagement().invalidSessionUrl("/logout");
-			// .and().addFilterAfter(new BasicAuthenticationFilter(authenticationManagerBean()), BasicAuthenticationFilter.class);
-
-			super.configure(http);
-		}
-
-		@Override
-		public void configure(WebSecurity web) throws Exception {
-
-			web.debug(true);
-		}
-	}
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	MyUserDetailsService userDetailsService;
 
-	protected void configureGlobal(AuthenticationManagerBuilder auth)
-			throws Exception {
-		auth.authenticationProvider(authenticationProvider());
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+				.authorizeRequests()
+				.antMatchers("/registration", "/").permitAll()
+				.antMatchers("/docent/**").hasRole("DOCENT")
+				.anyRequest().authenticated()
+				.and().httpBasic()
+				.and().sessionManagement()
+				.invalidSessionUrl("/logout");
+		// .and().addFilterAfter(new BasicAuthenticationFilter(authenticationManagerBean()), BasicAuthenticationFilter.class);
+
+		super.configure(http);
+	}
+
+	@Override
+	public void configure(WebSecurity web) {
+		web.debug(true);
 	}
 
 	@Bean
